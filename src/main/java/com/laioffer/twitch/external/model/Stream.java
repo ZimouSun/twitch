ff8bca252bc82685131923cf39d9b60e131f1ba3
ZimouSun <78500226+ZimouSun@users.noreply.github.com>
2023-09-16 18:52:51 -0400

    Class 9

---
+++ src/main/java/com/laioffer/twitch/external/model/Stream.java
@@ -0,0 +1,4 @@
+package com.laioffer.twitch.external.model;
+
+public class Stream {
+}
--- src/main/java/com/laioffer/twitch/hello/Person.java
+++ src/main/java/com/laioffer/twitch/hello/Person.java
@@ -1,7 +1,10 @@
 package com.laioffer.twitch.hello;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public record Person(
-        String name, String company,Address homeAddress, Book favoriteBook
+        String name, String company, @JsonProperty("home_address") Address homeAddress,
+        @JsonProperty("favorite_book") Book favoriteBook
 ) {
 
 }
